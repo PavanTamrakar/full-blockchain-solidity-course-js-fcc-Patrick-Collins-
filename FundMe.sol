ed5d396ca6d041b55a925fb6ced8226d5d67ed6b
Pavan Tamrakar <72463719+PavanTamrakar@users.noreply.github.com>
2022-06-21 23:38:48 +0530

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -1,1 +1,12 @@
+//SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+contract FundMe{
+
+uint256 public minimumUsd = 50;
+
+    function fund() public payable{
+        require(msg.value >= 1e18, "Didn't sent enough"); //1e18 == 1 * 10 ** 18 == 1000000000000000000 == 1eth 
+    }
+}
